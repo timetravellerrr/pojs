de432a99a73207706ee11a41d65a039abddceecd
Hanif <hanif.90@hotmail.com>
2019-10-28 01:52:10 +0800

    HFD-03: UI fixes

--- src/helper/Pokemon.js
+++ src/helper/Pokemon.js
@@ -1,24 +1,24 @@
 class Pokemon {
-    constructor(data) {
-      this.id = data.id;
-      this.name = data.name;
-      this.sprite = data.sprites.front_default;
-      this.sprites = data.sprites;
-      this.types = data.types;
-      this.height = data.height;
-      this.weight = data.weight;
-      this.abilities = data.abilities;
-      this.moves = data.moves;
-      this.base_experience = data.base_experience;
-      this.is_default = data.is_default;
-      this.order = data.order;
-      this.forms = data.forms;
-      this.game_indices = data.game_indices;
-      this.held_items = data.held_items;
-      this.location_area_encounters = data.location_area_encounters;
-      this.species = data.species;
-      this.stats = data.stats;
-    }
-  }
-  
-  export default Pokemon;
+	constructor(data) {
+		this.id = data.id;
+		this.name = data.name;
+		this.sprite = data.sprites.front_default;
+		this.sprites = data.sprites;
+		this.types = data.types;
+		this.height = data.height;
+		this.weight = data.weight;
+		this.abilities = data.abilities;
+		this.moves = data.moves;
+		this.base_experience = data.base_experience;
+		this.is_default = data.is_default;
+		this.order = data.order;
+		this.forms = data.forms;
+		this.game_indices = data.game_indices;
+		this.held_items = data.held_items;
+		this.location_area_encounters = data.location_area_encounters;
+		this.species = data.species;
+		this.stats = data.stats;
+	}
+}
+
+export default Pokemon;
--- src/utilities/Utils.js
+++ src/utilities/Utils.js
@@ -1,8 +1,9 @@
 class Utils {
-   
-  capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+
+	capitalize(str) {
+		// return str.charAt(0).toUpperCase() + str.slice(1);
+		return str.toLowerCase().split(' ').map(s => s.charAt(0).toUpperCase() + str.substring(1)).join(' ');
+	}
 }
 
 export default Utils;
